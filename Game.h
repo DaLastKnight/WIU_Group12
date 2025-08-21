3d05ebb995d533d62bbd859e1f407b014588d6cc
254460Z <254460Z@mymail.nyp.edu.sg>
2025-08-21 17:05:36 +0800

    Add files via upload

--- Game.h
+++ Game.h
@@ -2,17 +2,20 @@
 #include "World.h"
 #include "Town.h"
 #include "Dungeon.h"
+
 class Game
 {
 private:
 	World* currentWorld;
 	Town town;
 	Dungeon* dungeonPtr;
+
+	bool confirmSelection();
 public:
 	Game();
 	~Game();
 
 	void initGame();
+	void startingScreen();
 	void startGame();
 };
-
